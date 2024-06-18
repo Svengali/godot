c71155ae5dac0e61958b22cd7e8dfef32e7ddca4
Marc Hernandez <marcsh@gmail.com>
2024-06-18 15:14:03 -0700

    Add building message and instructions for C# build
    x) Added echo messages during build process
    x) Included command to generate mono glue for C#

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+eco BUILDING GODOT . . .
 scons debug_symbols=yes platform=macos arch=arm64 module_mono_enabled=yes CCFLAGS="-fno-omit-frame-pointer -fno-inline -ggdb3"
+./bin/godot.macos.editor.arm64.mono  --headless --generate-mono-glue modules/mono/glue
+echo BUILT 
+echo Use build2.sh to build the C#
