937977657a20d3619d1f6cd06865d9053940183f
Marc Hernandez <marcsh@gmail.com>
2024-06-27 13:49:47 -0700

    Update build scripts for Godot with additional build commands
    x) Added more 'echo BUILT' statements
    x) Included command to generate mono glue in build2.sh
    x) Updated paths and flags for building Godot on macOS

--- build2.sh
+++ build2.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+./bin/godot.macos.editor.arm64.mono  --headless --generate-mono-glue modules/mono/glue
 ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir ./bin --push-nupkgs-local /Users/marcsh/prj/tools/Nuget
