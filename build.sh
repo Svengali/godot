d3fc3f2ac22cdc8b1dc67f2609fd7245dcca6bbb
Marc Hernandez <marcsh@gmail.com>
2024-07-09 23:03:12 -0700

    Scripting updates

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 eco BUILDING GODOT . . .
-scons debug_symbols=yes platform=macos arch=arm64 module_mono_enabled=yes CCFLAGS="-fno-omit-frame-pointer -fno-inline -ggdb3"
+scons debug_symbols=yes platform=macos arch=arm64 module_mono_enabled=yes CCFLAGS="-fno-omit-frame-pointer -fno-inline -ggdb3" vulkan_sdk_path="/Users/marcsh/prj/tools/VulkanSDK/1.3.283.0"
 echo BUILT 
 echo BUILT 
 echo BUILT 
