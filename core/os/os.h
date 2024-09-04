5d423df41424c99503629d9d21b3fdc8727c817e
Marc Hernandez <marcsh@gmail.com>
2024-09-04 12:19:54 -0700

    Merge remote-tracking branch 'origin/master'

--- core/os/os.h
+++ core/os/os.h
@@ -75,6 +75,7 @@ class OS {
 	int _display_driver_id = -1;
 	String _current_rendering_driver_name;
 	String _current_rendering_method;
+	bool _is_gles_over_gl = false;
 
 	RemoteFilesystemClient default_rfs;
 
@@ -111,9 +112,6 @@ protected:
 	virtual void initialize() = 0;
 	virtual void initialize_joypads() = 0;
 
-	void set_current_rendering_driver_name(const String &p_driver_name) { _current_rendering_driver_name = p_driver_name; }
-	void set_current_rendering_method(const String &p_name) { _current_rendering_method = p_name; }
-
 	void set_display_driver_id(int p_display_driver_id) { _display_driver_id = p_display_driver_id; }
 
 	virtual void set_main_loop(MainLoop *p_main_loop) = 0;
@@ -131,8 +129,13 @@ public:
 
 	static OS *get_singleton();
 
+	void set_current_rendering_driver_name(const String &p_driver_name) { _current_rendering_driver_name = p_driver_name; }
+	void set_current_rendering_method(const String &p_name) { _current_rendering_method = p_name; }
+	void set_gles_over_gl(bool p_enabled) { _is_gles_over_gl = p_enabled; }
+
 	String get_current_rendering_driver_name() const { return _current_rendering_driver_name; }
 	String get_current_rendering_method() const { return _current_rendering_method; }
+	bool get_gles_over_gl() const { return _is_gles_over_gl; }
 
 	int get_display_driver_id() const { return _display_driver_id; }
 
